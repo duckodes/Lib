37ca723872ae71ef3cbb3e51a7582d7a61ebad76
BearHubs <bear90789078@gmail.com>
2023-11-04 18:31:19 +0800

    finalsettin
    fin

--- js/limitlib.js
+++ js/limitlib.js
@@ -1,5 +1,6 @@
 if (getParameterByName("v", window.location.href) !== "userlogin_privatelib_true") {
     if (localStorage.getItem("userlogin_privatelib_true") !== "lnv2kor@nkvof8mGo3pkE8s3v5g2e1d8c6sda&$SLgEMsdV"){
+        localStorage.removeItem("userlogin_privatelib_true");
         localStorage.setItem("userlogin_privatelib_fail", "true");
         window.location.href = "main.html";
     }
