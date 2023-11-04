f0be9087384c1ab502879672b794a2fccce04e2c
BearHubs <bear90789078@gmail.com>
2023-11-04 17:40:03 +0800

    someconsolehide
    consolehide

--- js/limitlibs.js
+++ js/limitlibs.js
@@ -1,4 +1,3 @@
-console.log(localStorage.getItem("userlogin_privatelib_true"));
 if (localStorage.getItem("userlogin_privatelib_true") !== "lnv2kor@nkvof8mGo3pkE8s3v5g2e1d8c6sda&$SLgEMsdV") {
     localStorage.setItem("userlogin_privatelib_fail", "true");
     window.location.href = "main.html";
