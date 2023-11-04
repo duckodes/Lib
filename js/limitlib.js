f23401546e68743574f0cd0f822d6004834407da
BearHubs <bear90789078@gmail.com>
2023-11-05 01:20:29 +0800

    lokw
    uplokw

--- js/limitlib.js
+++ js/limitlib.js
@@ -1,11 +1,23 @@
-if (getParameterByName("v", window.location.href) !== "privatelib_true_ugcid_@vklodO^vidanker") {
-    if (localStorage.getItem("userlogin_privatelib_true") !== "lnv2kor@nkvof8mGo3pkE8s3v5g2e1d8c6sda&$SLgEMsdV"){
-        localStorage.removeItem("userlogin_privatelib_true");
-        localStorage.setItem("userlogin_privatelib_fail", "true");
-        window.location.href = "main.html";
-    }
-}
-else {
-    localStorage.setItem("userlogin_privatelib_true", "lnv2kor@nkvof8mGo3pkE8s3v5g2e1d8c6sda&$SLgEMsdV");
-}
-window.history.pushState({}, "", "UGPrivateLibrary");
+fetch('Resource/Private.Link.net/private=link=end.ugc')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('fileError');
+        }
+        return response.text();
+    })
+    .then(text => {
+        if (getParameterByName("v", window.location.href) !== getParameterByName("v", text)) {
+            if (localStorage.getItem("userlogin_privatelib_true") !== "lnv2kor@nkvof8mGo3pkE8s3v5g2e1d8c6sda&$SLgEMsdV") {
+                localStorage.removeItem("userlogin_privatelib_true");
+                localStorage.setItem("userlogin_privatelib_fail", "true");
+                window.location.href = "main.html";
+            }
+        }
+        else {
+            localStorage.setItem("userlogin_privatelib_true", "lnv2kor@nkvof8mGo3pkE8s3v5g2e1d8c6sda&$SLgEMsdV");
+            window.history.pushState({}, "", "UGPrivateLibrary");
+        }
+    })
+    .catch(error => {
+        console.error(error);
+    });
