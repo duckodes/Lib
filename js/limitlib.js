2e6c1e73de1669a466d9a43186e3109759a7b07d
BearHubs <bear90789078@gmail.com>
2023-11-04 21:36:42 +0800

    somenew
    newfor

--- js/limitlib.js
+++ js/limitlib.js
@@ -1,4 +1,4 @@
-if (getParameterByName("v", window.location.href) !== "userlogin_privatelib_true") {
+if (getParameterByName("v", window.location.href) !== "userlogin_privatelib_true_ugcid_lnv2kor@nkvof8mGo3pkE8s3v5g2e1d8c6sda&$SLgEMsdV") {
     if (localStorage.getItem("userlogin_privatelib_true") !== "lnv2kor@nkvof8mGo3pkE8s3v5g2e1d8c6sda&$SLgEMsdV"){
         localStorage.removeItem("userlogin_privatelib_true");
         localStorage.setItem("userlogin_privatelib_fail", "true");
